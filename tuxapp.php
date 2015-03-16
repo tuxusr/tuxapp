c908c3e16523c6950891a68553e91744fbc69cd7
Tiago Floriano <tjfreelancer@mail.com>
2015-03-16 08:43:15 -0300

    Reorganização de arquivos.

--- tuxapp.php
+++ tuxapp.php
@@ -1,4 +1,11 @@
 <?
 session_start();
+$microtime = microtime();
 include("inc/core/config.php");
 include("inc/core/core.php");
+
+
+
+echo "P&aacute;gina carregada em ";
+print (number_format($microtime,2));
+echo " segundos";
